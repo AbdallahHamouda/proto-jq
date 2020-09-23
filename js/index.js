cc2a4934b2709429aad6aa8c3a76183d2f9b3e96
AbdallahHamouda <abdallahhamouda4@gmail.com>
2020-09-23 23:31:19 +0200

    content validation is done all is complete if you want more upgrade here make load screen

--- js/index.js
+++ js/index.js
@@ -6,11 +6,14 @@ var email= $(".email").val();
 var msg = $(".msg").val();
       if(name.length<3 || email.length<10 ||msg.length<5){
           console.log("error")
+          $(".notverify").removeClass("hides")
+          $(".verify").addClass("hides")
       }else {
-        
+        $(".verify").removeClass("hides")
+        $(".notverify").addClass("hides")
         axios.post('http://localhost:1337/Contacts',
         {name:name,email:email,msg:msg}).then(response => {
-        console.log(response);
+        console.log(response.status);
         console.log("success");
       });
       }
